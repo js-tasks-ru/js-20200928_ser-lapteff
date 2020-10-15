afa535ce8571cf2667bc91cf0668119ffc33c22b
serlapteff <ser.lapteff@yandex.ru>
2020-10-15 19:36:24 +0300

    Компонент SortableTable

--- 05-dom-document-loading/2-sortable-table-v1/index.js
+++ 05-dom-document-loading/2-sortable-table-v1/index.js
@@ -1,4 +1,107 @@
 export default class SortableTable {
+  element;
+
+  constructor(header = [], {data = []} = {}) {
+    this.header = header;
+    this.data = data;
+
+    this.renderComponetTemplate();
+    this.getTemplateBody(this.data);
+  }
+  sort(fieldValue, orderValue = 'asc') {
+    let newArr = [...this.data];
+    switch (orderValue) {
+    case 'desc':
+      newArr.sort( (a, b) => {
+        if (fieldValue !== "title") {
+          return Number(b[fieldValue]) - Number(a[fieldValue]);
+        } else {
+          return -1 * a[fieldValue].toString().localeCompare(b[fieldValue], ['ru', 'en'], {caseFirst: 'upper'});
+        }
+      });
+      return this.getTemplateBody(newArr);
+    case 'asc':
+    default:
+      newArr.sort( (a, b) => {
+        if (fieldValue !== "title") {
+          return Number(a[fieldValue]) - Number(b[fieldValue]);
+        } else {
+          return 1 * a[fieldValue].toString().localeCompare(b[fieldValue], ['ru', 'en'], {caseFirst: 'upper'});
+        }
+      });
+      return this.getTemplateBody(newArr);
+    }
+  }
+
+  getTamplateHeader(headers) {
+    if (headers && headers.length) {
+      const headerColumn = headers.map((elem) => {
+        if (elem.template) {
+          return elem.template();
+        } else {
+          return `
+            <div class="sortable-table__cell" data-id="${elem.id}" data-sortable="${elem.sortable}">
+              <span>${elem.title}</span>
+            </div>
+          `;
+        }
+      }).join('');
+      return `
+        <div data-element="header" class="sortable-table__header sortable-table__row">
+            ${headerColumn}
+        </div>
+      `;
+    }
+  }
+  getTemplateBody(data) {
+    if (data && data.length) {
+      const tableRows = data.map((elem) => {
+        return `<a href="/products/${elem.id}" class="sortable-table__row">
+            ${this.header.map(
+                item => {
+                  return (item.template) ? item.template(elem[item.id]) : `<div class="sortable-table__cell">${elem[item.id]}</div>`
+                }
+              ).join('')}
+          </a>
+        `;
+      }).join('');
+      const tableBody = this.element.querySelector('.sortable-table__body');
+
+      this.subElements = this.getSubElements(this.element);
+      return tableBody.innerHTML = tableRows;
+    }
+  }
+
+  renderComponetTemplate() {
+    const container = document.createElement('div');
+
+    container.innerHTML = `
+      <div data-element="productsContainer" class="products-list__container">
+          <div class="sortable-table">
+          ${this.getTamplateHeader(this.header)}
+        <div data-element="body" class="sortable-table__body"></div>
+        </div>
+      </div>
+    `;
+    this.element = container.firstElementChild;
+  }
+
+  getSubElements(element) {
+    const elements = element.querySelectorAll('[data-element]');
+
+    return [...elements].reduce((acc, item) => {
+      acc[item.dataset.element] = item;
+      return acc;
+    }, {});
+  }
+
+  remove() {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+  }
 
 }
 
